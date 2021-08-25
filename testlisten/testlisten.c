c86028ab92f8faa626f2a92ccf620348410b528c
sypzg <sypzg2021@outlook.com>
2021-08-25 21:30:30 +0800

    solve compile bug.

--- testlisten/testlisten.c
+++ testlisten/testlisten.c
@@ -6,13 +6,14 @@
 #include<assert.h>
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-static bool stop = False;
+static int  stop = 0;
 
 //signal handler
 static void handle_term(int sig)
 {
-	stop = True;
+	stop = 1;
 }
 
 int main( int argc, char* argv[] )
@@ -21,19 +22,19 @@ int main( int argc, char* argv[] )
 
 	if( argc <= 3)
 	{
-		printf("usage: %s ip_address port_number backlong\n", basename(argv[0]));
+		printf("usage: %s  ip_address port_number backlong\n",basename(argv[0]));
 	return 1;
 	}
-	const char* ip argv[1];
+	const char* ip = argv[1];
 	int port = atoi( argv[2] );
 	int backlog = atoi( argv[3] );
 	
-	int sock = socket( PF_INET， SOCK_STREAM， 0 );
+	int sock = socket( PF_INET, SOCK_STREAM, 0 );
 	assert( sock >= 0 );
 	//create a IPv4 socket
 	struct sockaddr_in address;
 	bzero( &address, sizeof( address ));
-	address.sin_faily = AF_INET;
+	address.sin_family = AF_INET;
 	inet_pton( AF_INET, ip, &address.sin_addr );
 	address.sin_port = htons( port );
 
@@ -42,9 +43,11 @@ int main( int argc, char* argv[] )
 
 	ret = listen( sock, backlog );
 	assert( ret != -1 );
-
-	while(!=stop)
+	
+	int cnt = 1;
+	while(!stop)
 	{
+	printf("sleep %d-th.",cnt++);
 	sleep(1);
 	}
 	close(sock);
