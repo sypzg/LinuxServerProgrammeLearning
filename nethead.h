58bf75d090fe636649de9f07a70fe1b876a3f3cd
syp <sypzg2021@outlook.com>
2021-08-26 10:43:15 +0800

    ADD head file referenced by netPro

---
+++ nethead.h
@@ -0,0 +1,12 @@
+#ifndef _NETHEAD_H_
+#define _NETHEAD_H_
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+
+#endif
